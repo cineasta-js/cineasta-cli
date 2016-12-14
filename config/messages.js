beedbacf5f8806aa207b44a243e5e8bff10d7b5f
lucasfs7 <lucasfs7@gmail.com>
2016-12-14 19:46:59 -0200

    validates installation of create-react-app and throw an error

--- config/messages.js
+++ config/messages.js
@@ -2,4 +2,5 @@ module.exports = {
   generatingProject: 'Generating project',
   copyingTemplate: 'Adding some magic',
   removingFiles: 'Cleaning up',
+  noCreateReactApp: 'We dependend on create-react-app to work, please install it.'
 }
