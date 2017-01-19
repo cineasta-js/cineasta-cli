b142490de408e35926d38cfacf6378e08c7440c1
lucasfs7 <lucasfs7@gmail.com>
2017-01-19 15:40:38 -0200

    fix provider template - space between name and props

--- template/provider.js
+++ template/provider.js
@@ -2,7 +2,7 @@ module.exports = ({ name, props }) => (
 
 `
 export const ${ name }Provider = (next) => (
-  <${ name }${ Object.keys(props).map(p => `${ p }={ ${ props[p] } }`).join(' ') }>
+  <${ name } ${ Object.keys(props).map(p => `${ p }={ ${ props[p] } }`).join(' ') }>
     { next }
   </${ name }>
 )
